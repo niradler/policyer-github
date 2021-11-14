d799415a5cd96b8fe56207f9d03e84cde67e3249
Nir Adler <niradler@users.noreply.github.com>
2021-11-14 23:48:36 +0200

    hide events

--- provider.js
+++ provider.js
@@ -20,7 +20,8 @@ class GithubProvider extends Provider {
       return acc;
     }, {});
     if (!events.env) events.env = githubEnv;
-    if (process.env.INPUT_VERBOSE === "true") console.log("events", events);
+    if (process.env.INPUT_VERBOSE === "true")
+      console.log("events", JSON.stringify(events));
     return events;
   }
 
