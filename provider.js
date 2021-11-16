864371aad2a1c4d22124acc4baa748ad3b2bca52
Nir Adler <niradler@users.noreply.github.com>
2021-11-17 01:28:09 +0200

    github action

--- provider.js
+++ provider.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const getKey = require("lodash.get");
 const { Provider } = require("policyer");
 const core = require("@actions/core");
@@ -9,27 +10,51 @@ class GithubProvider extends Provider {
 
   async collect(configuration) {
     try {
-      const authToken = core.getInput("github_token", { required: true });
+      let data = {};
       const eventName = github.context.eventName;
+
       core.info(`Event name: ${eventName}`);
       const validEvents = configuration.validEvents;
       if (validEvents.indexOf(eventName) < 0) {
         core.setFailed(`Invalid event: ${eventName}`);
         throw new core.Error(`Invalid event: ${eventName}`);
       }
-      const args = Object.keys(configuration.args).reduce((acc, key) => {
-        acc[key] = getKey(github, configuration.args[key]);
+
+      if (configuration.type == "rest") {
+        const authToken = core.getInput("github_token", { required: true });
+        const args = Object.keys(configuration.args).reduce((acc, key) => {
+          acc[key] = getKey(github, configuration.args[key]);
+          return acc;
+        }, {});
+        console.log({ args });
+        const client = github.getOctokit(authToken);
+        const res = await client[configuration.type][configuration.domain][
+          configuration.action
+        ](args);
+        data = res.data;
+      }
+
+      const env = Object.keys(process.env).reduce((acc, key) => {
+        if (key.startsWith("GITHUB_") || key.startsWith("INPUT_")) {
+          acc[key] = process.env[key];
+        }
         return acc;
       }, {});
-      console.log({ args });
-      const client = github.getOctokit(authToken);
-      const { data } = await client[configuration.type][configuration.domain][
-        configuration.action
-      ](args);
 
-      if (process.env.INPUT_VERBOSE === "true")
+      const event = JSON.parse(
+        fs.readFileSync(
+          process.env.GITHUB_EVENT_PATH || "events/example.json",
+          "utf8"
+        )
+      );
+
+      if (process.env.INPUT_VERBOSE === "true") {
         console.log("data", JSON.stringify(data));
-      return data;
+        console.log("env", JSON.stringify(env));
+        console.log("event", JSON.stringify(event));
+      }
+
+      return { data, env, event };
     } catch (error) {
       console.error(error);
       core.setFailed(error.message);
@@ -39,8 +64,8 @@ class GithubProvider extends Provider {
 
   async evaluate({ configuration, checks }) {
     if (configuration.provider == "github-provider") {
-      const todo = await this.collect(configuration);
-      const report = this.evaluateChecks(todo, checks);
+      const data = await this.collect(configuration);
+      const report = this.evaluateChecks(data, checks);
 
       return report;
     } else {
