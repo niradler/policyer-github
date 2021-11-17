32eb7dd52ef44267dc1a87fdf32759d39f9087a2
Nir Adler <niradler@users.noreply.github.com>
2021-11-17 23:54:41 +0200

    Fix test (#5)
    * add readme
    
    * add readme
    
    * add bin
    
    * add bin
    
    * add bin
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action
    
    * github action

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # policyer-github
 
-Github provider to test and validate github events.
+Github provider to test and validate github sdk calls.
 
 [visit policyer repository](https://github.com/niradler/policyer)
 
 ## Getting Started
 
 ```yaml
-# Add checks folder to your repository and create validate_branch.yml file
+# Create checks/validate_branch.yml file
 configuration:
   provider: github-provider
   type: github
 checks:
   - id: validate-branch-name
-    name: check if pr title start with PR.
+    name: check if branch start with Fix-.
     severity: High
     steps:
       - path: env.GITHUB_HEAD_REF
@@ -22,6 +22,32 @@ checks:
         utility: regex
         utilityProps:
           - "/^FIX-/"
+```
+
+```yaml
+# Create checks/validate_pr_title.yml file
+configuration:
+  provider: github-provider
+  type: rest
+  validEvents:
+    - pull_request
+  domain: pulls
+  action: get
+  args:
+    owner: context.payload.pull_request.base.user.login
+    repo: context.payload.pull_request.base.repo.name
+    pull_number: context.payload.pull_request.number
+checks:
+  - id: validate-pr-title
+    name: check if pr title start with FIX.
+    severity: High
+    steps:
+      - path: data.title
+        condition: equal
+        value: true
+        utility: regex
+        utilityProps:
+          - "/^FIX/i"
 ```
 
 and add github action:
@@ -38,10 +64,16 @@ jobs:
     steps:
       - uses: actions/checkout@v2
       - name: Policyer GitHub Action
-        uses: policyerorg/policyer-action@v0.0.2-alpha
+        uses: policyerorg/policyer-action@v0.0.3-alpha
         with:
           verbose: false
           provider: policyer-github
           internal: false
           checks_path: ./checks
 ```
+
+Links for github documentation:
+
+- https://docs.github.com/en/rest/reference
+- https://octokit.github.io/rest.js/v18
+-
--- provider.js
+++ provider.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
+const getKey = require("lodash.get");
 const { Provider } = require("policyer");
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 class GithubProvider extends Provider {
   constructor(name = "todo-provider") {
@@ -7,28 +10,63 @@ class GithubProvider extends Provider {
   }
 
   async collect(configuration) {
-    const events = JSON.parse(
-      fs.readFileSync(
-        process.env.GITHUB_EVENT_PATH || "events/example.json",
-        "utf8"
-      )
-    );
-    const githubEnv = Object.keys(process.env).reduce((acc, key) => {
-      if (key.startsWith("GITHUB_")) {
-        acc[key] = process.env[key];
+    try {
+      let data = {};
+      const eventName = github.context.eventName;
+
+      core.info(`Event name: ${eventName}`);
+      const validEvents = configuration.validEvents;
+      if (validEvents.indexOf(eventName) < 0) {
+        core.setFailed(`Invalid event: ${eventName}`);
+        throw new core.Error(`Invalid event: ${eventName}`);
       }
-      return acc;
-    }, {});
-    if (!events.env) events.env = githubEnv;
-    if (process.env.INPUT_VERBOSE === "true")
-      console.log("events", JSON.stringify(events));
-    return events;
+
+      if (configuration.type == "rest") {
+        const authToken = core.getInput("github_token", { required: true });
+        const args = Object.keys(configuration.args).reduce((acc, key) => {
+          acc[key] = getKey(github, configuration.args[key]);
+          return acc;
+        }, {});
+        console.log({ args });
+        const client = github.getOctokit(authToken);
+        const res = await client[configuration.type][configuration.domain][
+          configuration.action
+        ](args);
+        data = res.data;
+      }
+
+      const env = Object.keys(process.env).reduce((acc, key) => {
+        if (key.startsWith("GITHUB_") || key.startsWith("INPUT_")) {
+          acc[key] = process.env[key];
+        }
+        return acc;
+      }, {});
+
+      const event = JSON.parse(
+        fs.readFileSync(
+          process.env.GITHUB_EVENT_PATH || "events/example.json",
+          "utf8"
+        )
+      );
+
+      if (process.env.INPUT_VERBOSE === "true") {
+        console.log("data", JSON.stringify(data));
+        console.log("env", JSON.stringify(env));
+        console.log("event", JSON.stringify(event));
+      }
+
+      return { data, env, event };
+    } catch (error) {
+      console.error(error);
+      core.setFailed(error.message);
+      throw error;
+    }
   }
 
   async evaluate({ configuration, checks }) {
-    if (configuration.type == "github") {
-      const todo = await this.collect(configuration);
-      const report = this.evaluateChecks(todo, checks);
+    if (configuration.provider == "github-provider") {
+      const data = await this.collect(configuration);
+      const report = this.evaluateChecks(data, checks);
 
       return report;
     } else {
